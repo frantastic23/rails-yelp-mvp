a1bfdf663593386960a91efa15dbe32c927c62d3
Francesca Cartia <francescacartia@yahoo.it>
2022-01-19 17:10:57 +0100

    Yelp MVP done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  category = %w[chinese italian japanese french belgian].sample
+  restaurant = Restaurant.create!({ name: Faker::App.name, address: Faker::Address.street_address, phone_number:Faker::PhoneNumber.cell_phone, category: category })
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
